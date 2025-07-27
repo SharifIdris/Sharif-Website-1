c02b8a699282c74dc4fa1cd6877090d02a2806f3
Sharif idris <sharifidris8@gmail.com>
2025-07-27 23:04:44 +0000

    just put a thin line cayan glow around the certificate card

--- src/components/sections/certifications.tsx
+++ src/components/sections/certifications.tsx
@@ -48,7 +48,7 @@ export default function Certifications({ certifications }: CertificationsProps) 
               >
                 {/* Front of the card */}
                 <div 
-                    className="absolute inset-0 flex cursor-pointer flex-col items-center justify-center text-center [backface-visibility:hidden] overflow-hidden rounded-lg"
+                    className="absolute inset-0 flex cursor-pointer flex-col items-center justify-center text-center [backface-visibility:hidden] overflow-hidden rounded-lg drop-shadow-glow-primary"
                     onClick={() => handleCardClick(index)}
                 >
                     <Image 
@@ -89,4 +89,3 @@ export default function Certifications({ certifications }: CertificationsProps) 
     </section>
   );
 }
-
